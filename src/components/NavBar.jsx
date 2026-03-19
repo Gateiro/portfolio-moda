6b61315754fec774f823c8761ce5a9b48156ced3
Matheus Rodrigues  Vieira <matheus.rodriguesvieira@outlook.com>
2026-03-19 17:28:35 -0300

    refatoração

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+    return (
+        <div className="w-full h-full">
+            <div className=""> 
+                {/*Logo*/}
+                <img src="" alt="" />
+                {/*Menu*/}
+                <ol>
+                    <li>
+                        <a href="">Ínicio</a>
+                    </li>
+                </ol>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Navbar;
